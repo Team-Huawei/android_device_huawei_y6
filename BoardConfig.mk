ffd9894ed0e2dc9fdea0b80359304f04e9baf1c2
Carlos Arriaga <carlosarriagacm@gmail.com>
2016-09-30 17:19:58 -0500

    y6: disable vendor prima

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -218,23 +218,23 @@ TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_INCLUDE_CRYPTO := true
 TW_NO_SCREEN_TIMEOUT := true
 
-PRIMA_ROOT := vendor/qcom/opensource/wlan/prima
-PRIMA_MODULE:
-	$(hide) mkdir -p $(KERNEL_OUT)/$(PRIMA_ROOT)
-	$(hide) cp -f $(PRIMA_ROOT)/Kbuild $(KERNEL_OUT)/$(PRIMA_ROOT)/Makefile
-	$(hide) cp -rf $(PRIMA_ROOT)/CORE $(KERNEL_OUT)/$(PRIMA_ROOT)/CORE
-	$(hide) cp -rf $(PRIMA_ROOT)/riva $(KERNEL_OUT)/$(PRIMA_ROOT)/riva
-	$(hide) $(MAKE) $(MAKE_FLAGS) -C $(KERNEL_OUT) M=$(KERNEL_OUT)/$(PRIMA_ROOT) ARCH=$(TARGET_ARCH) $(KERNEL_CROSS_COMPILE) MODNAME=wlan CONFIG_PRONTO_WLAN=m WLAN_ROOT=$(PRIMA_ROOT) modules
-	$(hide) arm-eabi-strip --strip-debug $(KERNEL_OUT)/$(PRIMA_ROOT)/wlan.ko
-	$(hide) mkdir -p $(KERNEL_MODULES_OUT)/pronto
-	$(hide) cp -f $(KERNEL_OUT)/$(PRIMA_ROOT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
-	$(hide) if [ -L $(KERNEL_MODULES_OUT)/wlan.ko ]; then rm $(KERNEL_MODULES_OUT)/wlan.ko; fi
-	$(hide) ln -s /system/lib/modules/pronto/pronto_wlan.ko  $(KERNEL_MODULES_OUT)/wlan.ko
-TARGET_KERNEL_MODULES += PRIMA_MODULE
+#PRIMA_ROOT := vendor/qcom/opensource/wlan/prima
+#PRIMA_MODULE:
+#	$(hide) mkdir -p $(KERNEL_OUT)/$(PRIMA_ROOT)
+#	$(hide) cp -f $(PRIMA_ROOT)/Kbuild $(KERNEL_OUT)/$(PRIMA_ROOT)/Makefile
+#	$(hide) cp -rf $(PRIMA_ROOT)/CORE $(KERNEL_OUT)/$(PRIMA_ROOT)/CORE
+#	$(hide) cp -rf $(PRIMA_ROOT)/riva $(KERNEL_OUT)/$(PRIMA_ROOT)/riva
+#	$(hide) $(MAKE) $(MAKE_FLAGS) -C $(KERNEL_OUT) M=$(KERNEL_OUT)/$(PRIMA_ROOT) ARCH=$(TARGET_ARCH) $(KERNEL_CROSS_COMPILE) MODNAME=wlan CONFIG_PRONTO_WLAN=m WLAN_ROOT=$(PRIMA_ROOT) modules
+#	$(hide) arm-eabi-strip --strip-debug $(KERNEL_OUT)/$(PRIMA_ROOT)/wlan.ko
+#	$(hide) mkdir -p $(KERNEL_MODULES_OUT)/pronto
+#	$(hide) cp -f $(KERNEL_OUT)/$(PRIMA_ROOT)/wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
+#	$(hide) if [ -L $(KERNEL_MODULES_OUT)/wlan.ko ]; then rm $(KERNEL_MODULES_OUT)/wlan.ko; fi
+#	$(hide) ln -s /system/lib/modules/pronto/pronto_wlan.ko  $(KERNEL_MODULES_OUT)/wlan.ko
+#TARGET_KERNEL_MODULES += PRIMA_MODULE
 
-CORE_CTL_MODULE:
-	$(hide) cp -rf $(LOCAL_PATH)/core_ctl.ko $(KERNEL_MODULES_OUT)/
-TARGET_KERNEL_MODULES +=  CORE_CTL_MODULE
+#CORE_CTL_MODULE:
+#	$(hide) cp -rf $(LOCAL_PATH)/core_ctl.ko $(KERNEL_MODULES_OUT)/
+#TARGET_KERNEL_MODULES +=  CORE_CTL_MODULE
 
 
 # inherit from the proprietary version
