87241a82c646faee5f2d33bb546e1eccb3fe4058
Carlos Arriaga <carlosarriagacm@gmail.com>
2016-09-30 21:46:04 -0500

    y6: Switch to standalone libinit
    * No need to build the msm one

--- init/init_y6.cpp
+++ init/init_y6.cpp
@@ -36,18 +36,12 @@
 #include "log.h"
 #include "util.h"
 
-#include "init_msm.h"
-
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
+void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
     char model[110];
     FILE* fp;
     int rc;
-
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
 
     rc = property_get("ro.board.platform", platform);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
